94a029298f0dd87be0e67f66e1ee9782ab3a81fe
Antoine Beauvais-Lacasse <alacasse@cloudraker.com>
2018-11-16 17:15:00 -0500

    Add S3 config; update dependencies

--- config.js
+++ config.js
@@ -9,10 +9,16 @@ module.exports = envalid.cleanEnv(
         ADDRESS: host({ devDefault: 'localhost', default: '0.0.0.0' }),
         PORT: port({ devDefault: 3000 }),
 
-        APOS_SESSION_SECRET: str({ devDefault: 'CHANGEME' })
+        APOS_SESSION_SECRET: str({ devDefault: 'CHANGEME' }),
+
+        APOS_MONGODB_URI: str({ devDefault: undefined }),
+        APOS_S3_BUCKET: str({ devDefault: undefined }),
+        APOS_S3_KEY: str({ devDefault: undefined }),
+        APOS_S3_REGION: str({ devDefault: undefined }),
+        APOS_S3_SECRET: str({ devDefault: undefined })
     },
     {
         dotEnvPath: path.join(__dirname, '.env'),
-        strict: true
+        strict: false
     }
 );
