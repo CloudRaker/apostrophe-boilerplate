cc32f051dafa2d93141597cd479e04040ed2660a
Mat Janson Blanchet <mjanson@cloudraker.com>
2019-02-26 11:54:36 -0500

    Added documentation on how to use boilerplate

--- src/js/initialize.js
+++ src/js/initialize.js
@@ -2,6 +2,8 @@ var modulesList = [];
 
 function initAll() {
 
+  // import all *.js files at same level
+  // and `init()` them
   modulesList.forEach(function (m) {
     require('./' + m + '.js').init();
   });
